108bbad0797ae1a0c3676f8f970e702ebe5071be
WebbErrJF <kuba97fryc@gmail.com>
2022-11-14 16:18:29 +0100

    Added filter function
    The filter function is based on the built-in filter function.
    It has two parameters, condition and data.
    Condition is a parameter that determines with which condition the
    filter function will filter. The data is the data supplied to the filter.

--- fp/filter.py
+++ fp/filter.py
@@ -1,1 +1,23 @@
-import datetime
+data=[10,20,30,40,50,60,70,80,90,100]
+
+#function which is going to provide a condition to main function 
+def condition_func(value):
+    if value<50:
+        return True
+    return False
+
+def filter_func(condition,data,n=0,filtred_data=[]): # I took advantage of fact, that default arguments are evaluated only one (filtered_data)
+    if condition(data[n]):
+        filtred_data.append(data[n])
+        if n<len(data)-1:
+            n=n+1
+            filter_func(condition,data,n)
+    else:
+        if n<len(data)-1:
+            n=n+1
+            filter_func(condition,data,n)
+        else:
+            print(filtred_data)
+            n=0
+
+filter_func(condition_func,data)
