cc08a16664ec98bf308b3bb5fd318b38b0c6717f
WebbErrJF <kuba97fryc@gmail.com>
2022-11-14 15:50:12 +0100

    Fixed postorder walk
    Previously, postwalk was a class that inhaled from a tree.
    Postwalk was changed to a function that takes a tree object

--- structures/tree/postwalk.py
+++ structures/tree/postwalk.py
@@ -2,38 +2,13 @@
 # Travers the current node"S right subtree
 # Visit the current node
 
-class BTS:
-    def __init__(self, data): #creating the root
-        self.left_child = None
-        self.data = data
-        self.right_child = None
+from tree import tree
 
-    def insert(self,data): #adding new node to the tree
-        if data>self.data: #condition to keep tree as BST, any value that is greater than value of the root goes to the right 
-            if not self.right_child:
-                self.right_child=BTS(data)
-            else: #if node allready exists call recursion
-                self.right_child.insert(data)
-        else:   #condition to keep tree as BST, any value that is greater than value of the root goes to the right 
-            if not self.left_child:
-                self.left_child=BTS(data)
-            else: #if node allready exists call recursion
-                self.left_child.insert(data)
+def postorder_walk(node):
+    if node.left_child:
+        postorder_walk(node.left_child)
+    if node.right_child:
+        postorder_walk(node.right_child)
+    print(node.data)
 
-    def postorder_walk(self):
-        if self.left_child:
-            self.left_child.postorder_walk()
-        if self.right_child:
-            self.right_child.postorder_walk()
-        print(self.data)
-
-tree=BTS(25)
-tree.insert(20)
-tree.insert(22)
-tree.insert(19)
-tree.insert(21)
-tree.insert(23)
-tree.insert(27)
-tree.insert(26)
-tree.insert(28)
-tree.postorder_walk()
+postorder_walk(tree)
