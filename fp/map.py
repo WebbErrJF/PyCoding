702ae09c74870929cb0707568053d91ac7f727b5
WebbErrJF <kuba97fryc@gmail.com>
2022-11-16 12:25:51 +0100

    Map function added
    The map function was created based on the internal map function.
    It accepts 2 arguments:
    1) a function that returns the result from the requested operation
    2) data
    The function returns a list of mapped data

--- fp/map.py
+++ fp/map.py
@@ -1,1 +1,22 @@
+data=[10,20,30,40,50,60,70,80,90,100]
 
+#function, thats provide what kind of operation will be done in map function
+def some_addition(value):
+    return value+2
+
+
+def map_func(function,data,n=0,output=[]):
+    if n<len(data):
+        output.append(function(data[n]))
+        n+=1
+        return map_func(function,data,n) #return it is needed here, beacause it returns data from return below
+    elif n==len(data):
+        final_output=[]
+        for element in output:
+            final_output.append(element)
+        output.clear()
+        n=0
+        return final_output
+
+print(map_func(some_addition,data))
+print(map_func(some_addition,data))
