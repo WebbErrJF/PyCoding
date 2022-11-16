9e6764517a52f4ebd54eb55f63079b5afb9685c1
WebbErrJF <kuba97fryc@gmail.com>
2022-11-16 14:59:38 +0100

    Reduce function added
    The reduce function was created based on the internal reduce function.
    It accepts 2 arguments:
    1) a function that returns the result from the requested operation
    2) data
    The function returns a value of reduced data

--- fp/reduce.py
+++ fp/reduce.py
@@ -1,1 +1,14 @@
+data=[10,20,30,40,50,60,70,80,90,100]
+
+#function which is going to provide an operation to reduce function 
+def operation_func(value,instantaneous_value):
+    return value+instantaneous_value
+
+def reduce_func(function,data,n=0,instantaneous_value=0):
+    if n<len(data):
+        instantaneous_value=function(data[n],instantaneous_value)
+        n+=1
+        return reduce_func(function,data,n,instantaneous_value) #return it is needed here, beacause it returns data from return below
+    elif n==len(data):
+        return instantaneous_value
 
