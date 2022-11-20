d7f8d3a53c653631ed125dcda88988616f0fa4b3
WebbErrJF <kuba97fryc@gmail.com>
2022-11-20 02:52:42 +0100

    Refactor the filter function
    The function worked only for numbers. Fix it
    by changing 'instantaneous_value' type to None.
    
    Remove iteration approach. After this change
    function is fully based on recursion.
    
    Get rid of 'condition function' in favor of
    lambda function.

--- fp/reduce.py
+++ fp/reduce.py
@@ -1,14 +1,17 @@
 data=[10,20,30,40,50,60,70,80,90,100]
+data2=["a","b","c","d","e"]
 
-#function which is going to provide an operation to reduce function 
-def operation_func(value,instantaneous_value):
-    return value+instantaneous_value
 
-def reduce_func(function,data,n=0,instantaneous_value=0):
-    if n<len(data):
-        instantaneous_value=function(data[n],instantaneous_value)
-        n+=1
-        return reduce_func(function,data,n,instantaneous_value) #return it is needed here, beacause it returns data from return below
-    elif n==len(data):
+def reduce_func(function,data,instantaneous_value=None):
+    if len(data)>=1:
+        if instantaneous_value is None:
+            instantaneous_value=data[0]
+            return reduce_func(function,data[1:],instantaneous_value)
+        else:
+            instantaneous_value=function(data[0],instantaneous_value)
+            return reduce_func(function,data[1:],instantaneous_value) #return it is needed here, beacause it returns data from return below
+    else:
         return instantaneous_value
 
+print(reduce_func(lambda instantaneous_value, value: value + instantaneous_value,data))
+print(reduce_func(lambda instantaneous_value, value: value + instantaneous_value,data2))
