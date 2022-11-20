75d0e75a48c2772812d53efd84cfb6a1d4270aaa
WebbErrJF <kuba97fryc@gmail.com>
2022-11-20 02:51:52 +0100

    Refactor the map function
    Remove iteration approach. After this change
    function is fully based on recursion.
    
    Get rid of 'condition function' in favor of
    lambda function.

--- fp/map.py
+++ fp/map.py
@@ -1,22 +1,16 @@
 data=[10,20,30,40,50,60,70,80,90,100]
 
-#function, thats provide what kind of operation will be done in map function
-def operation(value):
-    return value+2
+def map_func(function,data,maped_list=[]):
+    if len(data)>=1:
+        maped_list.append(function(data[0]))
+        return map_func(function,data[1:]) #return it is needed here, beacause it returns data from return below
+    else:
+        output=maped_list.copy()
+        maped_list.clear()
+        return output
 
 
-def map_func(function,data,n=0,output=[]):
-    if n<len(data):
-        output.append(function(data[n]))
-        n+=1
-        return map_func(function,data,n) #return it is needed here, beacause it returns data from return below
-    elif n==len(data):
-        final_output=[]
-        for element in output:  #added this "for" because by using clear, I am removing references, so final_output would also = "[]"
-            final_output.append(element)
-        output.clear()
-        n=0
-        return final_output
+print(map_func(lambda value: value+2,data))
+print(map_func(lambda value: value*2,data))
 
-print(map_func(operation,data))
-print(map_func(operation,data))
+
