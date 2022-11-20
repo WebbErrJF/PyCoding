f472377796f7b383b6e34907956bf2cfb6682519
WebbErrJF <kuba97fryc@gmail.com>
2022-11-20 17:47:45 +0100

    Refactor the filter function
    Remove 'filtred_data' and extend
    return statement with concat function

--- fp/filter.py
+++ fp/filter.py
@@ -1,19 +1,14 @@
-data=[10,5,20,30,40,50,60,70,80,90,100]
+data=[10,5,20,100,30,40,50,60,70,80,90,100]
+data2=["K","g","g","w"]
 
-#function which is going to provide a condition to main function 
-
-def filter_func(condition,data,filtred_data=[]): # I took advantage of fact, that default arguments are evaluated only one (filtered_data)
+def filter_func(condition,data):
     if len(data)>=1:
         if condition(data[0]):
-            filtred_data.append(data[0])
-            return filter_func(condition,data[1:])
+            return [data[0]] + filter_func(condition,data[1:])
         else:
             return filter_func(condition,data[1:])  
     else:
-        output=filtred_data.copy()
-        filtred_data.clear()
-        return output 
+        return []
 
 print(filter_func(lambda value: value<50,data))
-print(filter_func(lambda value: value<50,data))
-
+print(filter_func(lambda value: value=="g",data2))
